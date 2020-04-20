dd6736630abf0a1bcfe7b62c9d9221b8a16fddfa
Conner Vieira <vieirast@icloud.com>
2020-04-20 09:07:11 -0400

    Initial Commit

---
+++ Lix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lix
+//
+//  Created by Conner Vieira on 4/20/20.
+//  Copyright © 2020 V0LT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
